7dead7563b996d5825562a2a9ee0ce6e5b9f78de
ap <alpe88.junk@gmail.com>
2015-04-27 18:38:59 -0700

    ...wait for it

---
+++ themes/testtheme/functions.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Theme Name: Aleksandar Petrovic's WordPress Demo Theme
+Author: Aleksandar Petrovic
+Author URI: http://www.alna-dev.com
+Description: This is a demo theme I developed for the WEB170 class.
+Version: 1.0
+*/
+?>

623d71a79fbb61d82fe4dc8749938186d4658aa4
bossmanstunna <alpe88.dev@gmail.com>
2015-05-11 19:05:10 -0700

    introducing widgets
    ...!

--- themes/testtheme/front-page.php
+++ themes/testtheme/front-page.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 <!-- header.php Ends Here -->
 
-<!-- Content -->
+<!-- Content Begins Here -->
 <div class="nopadding jumbotron">
 	<div id="" class="flexslider" style="margin-top:2em;">
 		<ul class="slides">
@@ -27,11 +27,11 @@
 		<h1 class="col-xs-12">~~>a humbly amazing portfolio</h1>
   	</div>
 	
-	<!-- Begin Icon Navigation -->
 	<div class="row">
 		<div class="col-xs-12">	
 			<div class="row">
 				<div class="col-lg-12 col-xs-12">
+				<!-- Begin Icon Navigation -->
 					<div class="row" id="grid">
 						<div class="icon-row">
 							<div class="col-sm-1 col-xs-4 ico-menu-item"><a class="" href="../wordpress/" title="~>back to Home"><img src="<?php bloginfo('template_directory');?>/images/icons/back-button.png" class="img-thumbnail img-responsive" /></a></div>  
@@ -54,51 +54,40 @@
 			<!-- Main Content Section Begins Here -->
 			<div class="row">
 				<div class="col-xs-12 col-lg-12">
-				
 					<div id="loadedContent" class="col-xs-12 col-sm-8"><!-- Content will be loaded here -->
-						<?php if(have_posts()) : while(have_posts()) : the_post(); #begins the WP Loop ?>
-						<?php if(is_front_page() && is_home()){#when default home page ?>
-						<?php			the_content(); #display page content ?>
-						<?php	 }elseif (is_front_page()){#when static home page ?>
-										<?php the_content(); #display page content ?>
-						<?php	 }elseif (is_home()){#blog posts page as set up in Settings>>Reading ?>
-										<article id="post-excerpt-<?php the_ID(); ?>" class="post-excerpt">
-														<h2><a href="<?php the_permalink(); #link to post ?>"><?php the_title(); #title ?></a></h2>
-														<small>Posted on <?php the_time('F j, Y'); #time item posted on ?> by <?php the_author(); #post author ?>
-														in <?php the_category(', '); #list of categories ?></small>
-														<a href="<?php the_permalink(); #link ?>"><?php the_post_thumbnail( 'thumbnail' ); #featured image ?></a>
-														<?php the_excerpt(); #the excerpt of the post ?>
-														<p class="read-more">
-															<a href="<?php the_permalink(); #link to post, used to show full post ?>">Read More &raquo;</a>
-														</p>
-											</article>
-						<?php	 }else if(is_page()){#when it's a page ?>
-						<?php 				the_content(); #the content of the post ?>	
-						<?php	 }else{#possibly the sloppiest way to handle what I'm trying to do here.... ?>
-										<article id="post-content-<?php the_ID(); ?>" class="post-content">
-														<h2><a href="<?php the_permalink(); #link to post ?>"><?php the_title(); #title ?></a></h2>
-														<small>Posted on <?php the_time('F j, Y'); #time item posted on ?> by <?php the_author(); #post author ?>
-														in <?php the_category(', '); #list of categories ?></small>
-														<a href="<?php the_permalink(); #link ?>"><?php the_post_thumbnail( 'large' ); #featured image ?></a>
-														<?php the_content(); #the content of the post ?>
-														<p class="read-more">
-															<a href="../blog/">Back to Blog</a>
-														</p>
-										</article> 
-						<?php			} ?>
-						<?php endwhile; endif; #end of The Loop ?>
-					</div>
+					<!-- Widgets Start Here -->
+						<!-- Introducing Multiple Loops Here -->
+						<div class="row">
+							<div id="widgets" class="col-xs-12">
+								<!-- Loop One: -->
+								<section class="widget-item col-xs-6">
+									<?php if(have_posts()) : while(have_posts()) : the_post(); #begins the WP Loop ?>
+									<?php		the_content(); #display page content ?>
+									<?php endwhile; endif; #end of The Loop ?>
+								</section>
+								<!-- Loop Two: -->
+								<section class="widget-item col-xs-6">
+									<?php rewind_posts(); #this function stops the first loop in order to prevent a functional error - aka an infinite loop :[] ?>
+									<?php query_posts('showposts=3'); #this instructs the second loop - how many posts to pull from the blog ?>
+										<ul id="latest-posts">
+											<?php while (have_posts()) : the_post(); #the secound loop starts here ?>
+												<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+											<?php endwhile; #end of secound loop ?>
+										</ul>
+								</section>
+								<!-- Another Loop Might Go Here: -->
+							</div>
+						</div>
+    				</div>
 					<div id="sidebar" class="col-sm-4 hidden-xs"><!-- Sidebar will be loaded here -->
 						<?php get_sidebar(); ?>
 					</div>
 					<!-- Main Content Section Ends Here -->
-    			</div>
-			</div>
+				</div>
+			</div><small>front-page.php</small>
 		</div>
-	</div>
-<small>front-page.php</small>
 </div>
-<!-- Content -->
+<!-- Content Ends Here -->
 
 <!-- footer.php begins here -->
 <?php get_footer(); ?>

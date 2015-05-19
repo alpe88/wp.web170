6ae2e3d712404012be5a2f22ff96b12a32631019
cluetrain <greenstrat67@gmail.com>
2015-05-18 19:01:45 -0700

    cleanup

--- themes/testtheme/footer.php
+++ themes/testtheme/footer.php
@@ -40,7 +40,6 @@
 
   	<script type="text/javascript">
 		$(document).ready(function() {
-			var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
 			$('.flexslider').flexslider({
 				animation: "fade",
 				controlNav: false,
@@ -50,17 +49,6 @@
 			$('div.ico-menu-item a img').each(function(){
 				$(this).hover(function(){$(this).addClass("highlight");},function(){$(this).removeClass("highlight");});
 			});
-		    	$(window).scroll(function (event) {
-    				var y = $(this).scrollTop();
-    				if(y > 0){ $('#nav_wrap').slideUp(100); } 
-    				else{ $('#nav_wrap').slideDown(100); }
-			});
-			$(window).resize(function () {
-   				waitForFinalEvent(function(){
-    				}, 500, "this string is really unique...");
-				if(w < 768){ $('.ico-row').addClass("ico-row-style");}
-				if(w > 768){ $('.ico-row').removeClass("ico-row-style");}
-			});
 		});
 	</script>
 <?php wp_footer(); ?>
@@ -69,82 +57,3 @@
 <!-- Body Ends Here -->
 </html>
 <!-- Document Ends Here -->
-
-
-<?php
-/*
-<!-- Start New Menu --> 
-<div id="new-menu">
-	<p>I am working on the new responsive navigation system's menu function below:</p>
-    <ul id="new-menu-items">
-    <?php	
-	$main = get_pages(array('meta_key' => 'navigation', 'meta_value' => 'main', ));
-	$parent_ID = wp_get_post_parent_id( $post_ID );
-	
-	foreach ($main as $post) { // foreach gateway page... 
-	
-		if (is_page($post->ID)) { // get list item with classes of...
-			 
-			echo '<li class="page-item-'.$post->ID.' current-page-item">'; // ... current page item
-			 
-		} elseif ($parent_ID == ($post->ID)) {
-			
-			echo '<li class="page-item-'.$post->ID.' current-page-parent">'; // ... current page parent
-				
-		} else {
-			
-			echo '<li class="page-item-'.$post->ID.'">'; // ... none
-		}
-		
-		echo '<a href="'.get_permalink($post->ID).'">'.$post->post_title.'</a>'; // get the title with permalink
-		echo '<ul class="sub-menu">'; // get the sub-menu items
-			
-		if ($post->post_parent) { // if the page has a parent...
-						
-			echo '<li class="pagenav" >'.$post->post_title.' &raquo; Class';
-			echo '<ul>';
-			echo '<li><a href="'.get_permalink($post->post_parent).'">Syllabus</a></li>'; // stick in the link to syllabus with no class
-			wp_list_pages(array('child_of' => $post->post_parent, 'title_li' => '', 'meta_key' => 'navigation', 'meta_value' => 'class',)); 
-			echo '</ul>';
-			wp_list_pages(array('child_of' => $post->post_parent, 'title_li' => $post->post_title.' &raquo; Lectures:', 'meta_key' => 'navigation', 'meta_value' => 'lecture',));
-			wp_list_pages(array('child_of' => $post->post_parent, 'title_li' => $post->post_title.' &raquo; Assignments:', 'meta_key' => 'navigation', 'meta_value' => 'assignment',)); 
-			wp_list_pages(array('child_of' => $post->post_parent, 'title_li' => $post->post_title.' &raquo; Exercises:', 'meta_key' => 'navigation', 'meta_value' => 'exercise',));
-			wp_list_pages(array('child_of' => $post->post_parent, 'title_li' => $post->post_title.' &raquo; Teams:', 'meta_key' => 'navigation', 'meta_value' => 'team',));
-			wp_list_pages(array('child_of' => $post->post_parent, 'title_li' => $post->post_title.' &raquo; Students:', 'meta_key' => 'navigation', 'meta_value' => 'student',));
-			
-		} else { // if the page does not have a parent...
-		
-			echo '<li class="pagenav">'.$post->post_title.' &raquo; Class';
-			echo '<ul>';
-			
-			if (is_page($post->ID)) { // stick in the link to syllabus with class
-				
-				echo '<li class="current-page-item"><a href="'.get_permalink($post->post_parent).'">Syllabus</a></li>';
-				
-			} else {
-				
-				echo '<li><a href="'.get_permalink($post->post_parent).'">Syllabus</a></li>';
-				
-			}
-			
-			wp_list_pages(array('child_of' => $post->ID, 'title_li' => '', 'meta_key' => 'navigation', 'meta_value' => 'class',));
-			echo '</ul>';
-			wp_list_pages(array('child_of' => $post->ID, 'title_li' => $post->post_title.' &raquo; Lectures:', 'meta_key' => 'navigation', 'meta_value' => 'lecture',));
-			wp_list_pages(array('child_of' => $post->ID, 'title_li' => $post->post_title.' &raquo; Assignments:', 'meta_key' => 'navigation', 'meta_value' => 'assignment',));
-			wp_list_pages(array('child_of' => $post->ID, 'title_li' => $post->post_title.' &raquo; Exercises:', 'meta_key' => 'navigation', 'meta_value' => 'exercise',));
-			wp_list_pages(array('child_of' => $post->ID, 'title_li' => $post->post_title.' &raquo; Teams:', 'meta_key' => 'navigation', 'meta_value' => 'team',));
-			wp_list_pages(array('child_of' => $post->ID, 'title_li' => $post->post_title.' &raquo; Students:', 'meta_key' => 'navigation', 'meta_value' => 'student',));
-			
-		}
-		
-		echo '</li>';
-		echo '</ul>';
-	 
-	}
-	?>
-    </ul>
-</div>
-<!-- End New menu -->
-
-*/
-?>
